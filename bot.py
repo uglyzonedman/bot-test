048c2d6024663f9b061a80da79d6de63aff0fb9c
-_- <majest228@gmail.com>
2024-08-01 14:23:30 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,21 @@
-from aiogram import Bot, types, Dispatcher
+import asyncio
+from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
-from aiogram.utils import executor
 
-# Initialize bot and dispatcher
 bot = Bot(token="6772658669:AAFkfqaDMZL1LBBjMg8-9kXtAQaGIFVMYgs")
+
 dp = Dispatcher()
 
 
-# Define handlers
-@dp.message(commands=["start"])
-async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nНапиши мне что-нибудь!")
+@dp.message(F.text == "/start")
+async def start(message: Message):
+    await message.answer("tst")
 
 
-@dp.message(commands=["help"])
-async def process_help_command(message: types.Message):
-    await message.reply("Напиши мне что-нибудь, и я отправлю этот текст тебе в ответ!")
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
 
 
-@dp.message()
-async def echo_message(message: types.Message):
-    await bot.send_message(message.from_user.id, message.text)
-
-
-# Setup executor
 if __name__ == "__main__":
-    executor.start_polling(dp)
+    asyncio.run(main())
