1cd4c156fd52b5fba0836eb9a144492ea7036449
-_- <majest228@gmail.com>
2024-08-01 14:32:00 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,38 +1,11 @@
-# import asyncio
-# from aiogram import Bot, Dispatcher, F
-# from aiogram.types import Message
-
-# bot = Bot(token="6772658669:AAFkfqaDMZL1LBBjMg8-9kXtAQaGIFVMYgs")
-
-# dp = Dispatcher()
-
-
-# @dp.message(F.text == "/start")
-# async def start(message: Message):
-#     await message.answer("tst")
-
-
-# async def main():
-#     await bot.delete_webhook(drop_pending_updates=True)
-#     await dp.start_polling(bot)
-
-
-# if __name__ == "__main__":
-#     asyncio.run(main())
-
-
 import asyncio
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
 from aiohttp import ClientSession, TCPConnector
-from aiohttp.client import ClientSession as AiohttpClientSession
 
-
-class CustomSession(ClientSession):
-    def __init__(self, **kwargs):
-        connector = TCPConnector(ssl=False)  # Отключаем SSL
-        super().__init__(connector=connector, **kwargs)
-
+# Настройка клиента aiohttp с отключенным SSL
+connector = TCPConnector(ssl=False)
+session = ClientSession(connector=connector)
 
 bot = Bot(token="6772658669:AAFkfqaDMZL1LBBjMg8-9kXtAQaGIFVMYgs", session=CustomSession)
 dp = Dispatcher()
