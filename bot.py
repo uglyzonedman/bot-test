0d8e7709d3c148b0967b212ccd68b20ced80ff6b
-_- <majest228@gmail.com>
2024-08-01 14:25:50 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,40 @@
+# import asyncio
+# from aiogram import Bot, Dispatcher, F
+# from aiogram.types import Message
+
+# bot = Bot(token="6772658669:AAFkfqaDMZL1LBBjMg8-9kXtAQaGIFVMYgs")
+
+# dp = Dispatcher()
+
+
+# @dp.message(F.text == "/start")
+# async def start(message: Message):
+#     await message.answer("tst")
+
+
+# async def main():
+#     await bot.delete_webhook(drop_pending_updates=True)
+#     await dp.start_polling(bot)
+
+
+# if __name__ == "__main__":
+#     asyncio.run(main())
+
+
 import asyncio
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
+from aiohttp import ClientSession, TCPConnector
+from aiohttp.client import ClientSession as AiohttpClientSession
 
-bot = Bot(token="6772658669:AAFkfqaDMZL1LBBjMg8-9kXtAQaGIFVMYgs")
 
+class CustomSession(ClientSession):
+    def __init__(self, **kwargs):
+        connector = TCPConnector(ssl=False)  # Отключаем SSL
+        super().__init__(connector=connector, **kwargs)
+
+
+bot = Bot(token="6772658669:AAFkfqaDMZL1LBBjMg8-9kXtAQaGIFVMYgs", session=CustomSession)
 dp = Dispatcher()
 
 
@@ -13,8 +44,11 @@ async def start(message: Message):
 
 
 async def main():
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    try:
+        await bot.delete_webhook(drop_pending_updates=True)
+        await dp.start_polling(bot)
+    except Exception as e:
+        print(f"Error: {e}")
 
 
 if __name__ == "__main__":
