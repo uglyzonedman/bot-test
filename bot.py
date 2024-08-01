5bde5f813bdf3a4fc9448171c90102a9067d90ae
-_- <majest228@gmail.com>
2024-08-01 14:19:31 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,28 @@
-from aiogram import Bot, types
-from aiogram.fsm import Router
+from aiogram import Bot, types, Dispatcher
+from aiogram.types import Message
 from aiogram.utils import executor
 
-# Initialize bot and router
+# Initialize bot and dispatcher
 bot = Bot(token="6772658669:AAFkfqaDMZL1LBBjMg8-9kXtAQaGIFVMYgs")
-router = Router()
+dp = Dispatcher()
 
 
 # Define handlers
-@router.message(commands=["start"])
+@dp.message(commands=["start"])
 async def process_start_command(message: types.Message):
     await message.reply("Привет!\nНапиши мне что-нибудь!")
 
 
-@router.message(commands=["help"])
+@dp.message(commands=["help"])
 async def process_help_command(message: types.Message):
     await message.reply("Напиши мне что-нибудь, и я отправлю этот текст тебе в ответ!")
 
 
-@router.message()
+@dp.message()
 async def echo_message(message: types.Message):
     await bot.send_message(message.from_user.id, message.text)
 
 
 # Setup executor
 if __name__ == "__main__":
-    from aiogram import Dispatcher
-
-    dp = Dispatcher()
-    dp.include_router(router)
     executor.start_polling(dp)
