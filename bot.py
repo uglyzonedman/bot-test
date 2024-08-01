adb963d5764fb6241b39dbda5d4be85ab7b58a37
-_- <majest228@gmail.com>
2024-08-01 14:36:28 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import asyncio
 from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
 from aiohttp import ClientSession, ClientTimeout, TCPConnector
+from aiogram.client.session.aiohttp import AiohttpSession
 
 
 class CustomAiohttpSession(AiohttpSession):
